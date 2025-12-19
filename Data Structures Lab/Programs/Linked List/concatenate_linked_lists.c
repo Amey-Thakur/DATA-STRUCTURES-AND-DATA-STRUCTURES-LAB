f10265f4a0554dbd3daa024930cd0a7324305e0c
Amey-Thakur <ameythakur20@gmail.com>
2025-12-18 22:06:33 -0500

    DS

--- Data Structures Lab/Programs/Linked List/concatenate_linked_lists.c
+++ Data Structures Lab/Programs/Linked List/concatenate_linked_lists.c
@@ -1,143 +1,181 @@
-#include<stdio.h>
-#include<stdlib.h>
+/*
+ * Program: Concatenate Two Linked Lists
+ * Description: Merges two linked lists into a single list
+ * Author: Amey Thakur
+ * Purpose: Demonstrates list concatenation and traversal
+ */
 
-struct node
-{
-	int data;
-	struct node* next;
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
 };
 
-struct node *one = NULL, *two = NULL, *result = NULL;
+// Function prototypes
+struct Node* createNode(int value);
+void insertNode(struct Node** head, int value);
+struct Node* concatenateLists(struct Node* list1, struct Node* list2);
+void displayList(struct Node* head, const char* listName);
+void freeList(struct Node* head);
 
-struct node* insertNode(int);
-void display();
-
-void main()
-{
-	int val=0,no=0,i=0;
-	struct node* ptr = NULL;
-	printf("Enter number of nodes to be entered in list 1 :  ");
-	scanf("%d",&no);
-	for(i=0;i<no;i++)
-	{
-		printf("Enter node %d in lst list :  ",i);
-		scanf("%d",&val);
-		if(one == NULL)
-		{
-			one = insertNode(val);
-		}
-		else
-		{
-			struct node* p;
-			p = one;
-		
-			while(p->next != NULL)
-			{
-				p = p->next;
-			}
-			p->next = insertNode(val);
-		}	
-	}
-	
-	printf("Enter number of nodes to be entered in list 2 :  ");
-	scanf("%d",&no);
-	for(i=0;i<no;i++)
-	{
-		printf("Enter node %d in 2nd list :  ",i);
-		scanf("%d",&val);
-		if(two == NULL)
-		{
-			two = insertNode(val);
-		}
-		else
-		{
-			struct node* p;
-			p = two;
-		
-			while(p->next != NULL)
-			{
-				p = p->next;
-			}
-			p->next = insertNode(val);
-		}	
-	}
-	ptr = one;
-	while(ptr != NULL)
-	{
-		val = ptr->data;
-		if(result == NULL)
-		{
-			result = insertNode(val);
-		}
-		else
-		{
-			struct node* p;
-			p = result;
-		
-			while(p->next != NULL)
-			{
-				p = p->next;
-			}
-			p->next = insertNode(val);
-		}
-		ptr = ptr->next;
-	}
-	ptr = two;
-	while(ptr != NULL)
-	{
-		val = ptr->data;
-		if(result == NULL)
-		{
-			result = insertNode(val);
-		}
-		else
-		{
-			struct node* p;
-			p = result;
-		
-			while(p->next != NULL)
-			{
-				p = p->next;
-			}
-			p->next = insertNode(val);
-		}
-		ptr = ptr->next;
-	}
-	
-	display();	
+int main() {
+    struct Node* list1 = NULL;
+    struct Node* list2 = NULL;
+    struct Node* mergedList = NULL;
+    int n, value, i;
+    
+    printf("=== Concatenate Two Linked Lists ===\n");
+    
+    // Create first list
+    printf("\nEnter number of nodes for List 1: ");
+    scanf("%d", &n);
+    printf("Enter %d values for List 1:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("  Node %d: ", i + 1);
+        scanf("%d", &value);
+        insertNode(&list1, value);
+    }
+    
+    // Create second list
+    printf("\nEnter number of nodes for List 2: ");
+    scanf("%d", &n);
+    printf("Enter %d values for List 2:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("  Node %d: ", i + 1);
+        scanf("%d", &value);
+        insertNode(&list2, value);
+    }
+    
+    // Display original lists
+    displayList(list1, "List 1");
+    displayList(list2, "List 2");
+    
+    // Concatenate lists
+    mergedList = concatenateLists(list1, list2);
+    
+    // Display merged list
+    displayList(mergedList, "Merged List");
+    
+    // Free memory
+    freeList(mergedList);
+    
+    return 0;
 }
 
-void display()
-{
-	struct node *ptr;
-	
-	printf("\n1st Linkedlist :\n");
-	for(ptr = one;ptr != NULL; ptr = ptr->next)
-	{
-		printf("--> %d",ptr->data);
-	}
-	
-	
-	printf("\n2nd Linklist :\n");
-	for(ptr = two;ptr != NULL; ptr = ptr->next)
-	{
-		printf("--> %d",ptr->data);
-	}
-	
-	printf("\n Merged Linklist : \n");
-	for(ptr = result;ptr != NULL ;ptr = ptr->next)
-	{
-		printf("--> %d", ptr->data);
-	}
+/*
+ * Function: createNode
+ * Description: Creates a new node with given value
+ * Parameters: value - Data to store
+ * Returns: Pointer to new node
+ */
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    
+    newNode->data = value;
+    newNode->next = NULL;
+    
+    return newNode;
 }
 
-struct node* insertNode(int val)
-{
-	struct node* temp;
-	temp=(struct node*)malloc(sizeof(struct node));
-	
-	temp->data = val;
-	temp->next = NULL;
-	
-	return temp;
+/*
+ * Function: insertNode
+ * Description: Inserts node at end of list
+ * Parameters:
+ *   head - Pointer to head pointer
+ *   value - Data to insert
+ */
+void insertNode(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    
+    current->next = newNode;
+}
+
+/*
+ * Function: concatenateLists
+ * Description: Concatenates two linked lists
+ * Parameters:
+ *   list1 - First linked list
+ *   list2 - Second linked list
+ * Returns: Head of merged list
+ * Algorithm: Creates new list by copying all nodes from both lists
+ */
+struct Node* concatenateLists(struct Node* list1, struct Node* list2) {
+    struct Node* result = NULL;
+    struct Node* current;
+    
+    // Copy all nodes from list1
+    current = list1;
+    while (current != NULL) {
+        insertNode(&result, current->data);
+        current = current->next;
+    }
+    
+    // Copy all nodes from list2
+    current = list2;
+    while (current != NULL) {
+        insertNode(&result, current->data);
+        current = current->next;
+    }
+    
+    return result;
+}
+
+/*
+ * Function: displayList
+ * Description: Displays all nodes in the list
+ * Parameters:
+ *   head - Head of list
+ *   listName - Name to display
+ */
+void displayList(struct Node* head, const char* listName) {
+    if (head == NULL) {
+        printf("\n%s is empty!\n", listName);
+        return;
+    }
+    
+    printf("\n%s: ", listName);
+    struct Node* current = head;
+    
+    while (current != NULL) {
+        printf("%d", current->data);
+        if (current->next != NULL) {
+            printf(" --> ");
+        }
+        current = current->next;
+    }
+    printf(" --> NULL\n");
+}
+
+/*
+ * Function: freeList
+ * Description: Frees all allocated memory in list
+ * Parameters: head - Head of list to free
+ */
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    struct Node* next;
+    
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
 }
--- Data Structures Lab/Programs/Linked List/find_max_min.c
+++ Data Structures Lab/Programs/Linked List/find_max_min.c
@@ -1,73 +1,176 @@
-#include<stdio.h>
-#include<stdlib.h>
+/*
+ * Program: Find Maximum and Minimum in Linked List
+ * Description: Finds the maximum and minimum values in a linked list
+ * Author: Amey Thakur
+ * Purpose: Demonstrates traversal and comparison operations
+ */
 
-struct node
-{
-	int data;
-	struct node* next;
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
 };
 
-struct node *one = NULL;
+// Function prototypes
+struct Node* createNode(int value);
+void insertNode(struct Node** head, int value);
+void findMaxMin(struct Node* head, int* max, int* min);
+void displayList(struct Node* head);
+void freeList(struct Node* head);
 
-struct node* insertNode(int);
-void minMax();
-
-void main()
-{
-	int val=0,no=0,i=0;
-	struct node* ptr = NULL;
-	printf("Enter number of nodes to be entered in list :  ");
-	scanf("%d",&no);
-	for(i=0;i<no;i++)
-	{
-		printf("Enter node %d in list :  ",i);
-		scanf("%d",&val);
-		if(one == NULL)
-		{
-			one = insertNode(val);
-		}
-		else
-		{
-			struct node* p;
-			p = one;
-		
-			while(p->next != NULL)
-			{
-				p = p->next;
-			}
-			p->next = insertNode(val);
-		}	
-	}
-	minMax();
- }
-
-struct node* insertNode(int val)
-{
-	struct node* temp;
-	temp=(struct node*)malloc(sizeof(struct node));
-	
-	temp->data = val;
-	temp->next = NULL;
-	
-	return temp;
-}	
-
-void minMax()
-{
-	struct node* ptr = NULL;
-	int count=0,min=0,max=0;
-	min = max = one->data;
-	for (ptr = one;ptr != NULL;ptr = ptr->next)
-	{    
-        if(ptr->data < min)
-        {
-        	min = ptr->data;
-		}
-		if(ptr->data > max)
-		{
-			max = ptr->data;
-		}
+int main() {
+    struct Node* head = NULL;
+    int n, value, i, max, min;
+    
+    printf("=== Find Maximum and Minimum in Linked List ===\n");
+    
+    // Create linked list
+    printf("\nEnter number of nodes: ");
+    scanf("%d", &n);
+    
+    if (n <= 0) {
+        printf("Invalid number of nodes!\n");
+        return 1;
     }
-    printf("\n\n %d is the Minimum value in the entered link list",min);
-	printf("\n %d is the Maximum value in the entered link list",max);
+    
+    printf("Enter %d values:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("  Node %d: ", i + 1);
+        scanf("%d", &value);
+        insertNode(&head, value);
+    }
+    
+    // Display list
+    displayList(head);
+    
+    // Find max and min
+    findMaxMin(head, &max, &min);
+    
+    printf("\nMaximum value: %d\n", max);
+    printf("Minimum value: %d\n", min);
+    printf("Difference: %d\n", max - min);
+    
+    // Free memory
+    freeList(head);
+    
+    return 0;
+}
+
+/*
+ * Function: createNode
+ * Description: Creates a new node with given value
+ * Parameters: value - Data to store
+ * Returns: Pointer to new node
+ */
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    
+    newNode->data = value;
+    newNode->next = NULL;
+    
+    return newNode;
+}
+
+/*
+ * Function: insertNode
+ * Description: Inserts node at end of list
+ * Parameters:
+ *   head - Pointer to head pointer
+ *   value - Data to insert
+ */
+void insertNode(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    
+    current->next = newNode;
+}
+
+/*
+ * Function: findMaxMin
+ * Description: Finds maximum and minimum values in the list
+ * Parameters:
+ *   head - Head of list
+ *   max - Pointer to store maximum value
+ *   min - Pointer to store minimum value
+ */
+void findMaxMin(struct Node* head, int* max, int* min) {
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+    
+    // Initialize with first node's data
+    *max = head->data;
+    *min = head->data;
+    
+    struct Node* current = head->next;
+    
+    // Traverse and compare
+    while (current != NULL) {
+        if (current->data > *max) {
+            *max = current->data;
+        }
+        if (current->data < *min) {
+            *min = current->data;
+        }
+        current = current->next;
+    }
+}
+
+/*
+ * Function: displayList
+ * Description: Displays all nodes in the list
+ * Parameters: head - Head of list
+ */
+void displayList(struct Node* head) {
+    if (head == NULL) {
+        printf("\nList is empty!\n");
+        return;
+    }
+    
+    printf("\nLinked List: ");
+    struct Node* current = head;
+    
+    while (current != NULL) {
+        printf("%d", current->data);
+        if (current->next != NULL) {
+            printf(" --> ");
+        }
+        current = current->next;
+    }
+    printf(" --> NULL\n");
+}
+
+/*
+ * Function: freeList
+ * Description: Frees all allocated memory
+ * Parameters: head - Head of list to free
+ */
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    struct Node* next;
+    
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
 }
