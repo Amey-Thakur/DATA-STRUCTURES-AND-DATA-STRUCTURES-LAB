4a4ccc2d2883b0573f92fa052a4486560ea54f12
Amey Thakur <ameythakur20@gmail.com>
2025-12-28 03:58:30 -0500

    DS

---
+++ DS/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'ds-lab-v1';
+const ASSETS_TO_CACHE = [
+    'index.html',
+    'style.css',
+    'script.js',
+    'manifest.json',
+    'assets/icon-192.png',
+    'assets/icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching assets');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
